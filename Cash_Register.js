897814bde92e2a3be4a80d8ed9191c8369169483
Kamil Synowiec <1935002@brunel.ac.uk>
2021-10-28 11:12:58 +0100

    Update Cash_Register.js

--- Cash_Register.js
+++ Cash_Register.js
@@ -1,5 +1,46 @@
 function checkCashRegister(price, cash, cid) {
-  let change;
+  let totalCID=0;
+  let changearr=[];
+
+  let currencyUnit=[
+    ["ONE HUNDRED", 100],
+    ["TWENTY", 20],
+    ["TEN", 10],
+    ["FIVE", 5],
+    ["ONE", 1],
+    ["QUARTER", 0.25],
+    ["DIME", 0.1],
+    ["NICKEL", 0.05],
+    ["PENNY", 0.01]
+  ];
+
+  
+  for(let property of cid)
+  {
+    totalCID+=property[1];
+  }
+
+  let changeToPay=cash-price;
+
+  if(changeToPay>totalCID)
+  {
+    return {status: "INSUFFICIENT_FUNDS", change: []};
+  }
+  else if(changeToPay === totalCID) 
+  {
+    return { status: "CLOSED", change: cid };
+  }
+  else
+  {
+    
+  }
+
+  
+
+
+
+
+
   return change;
 }
 
